16e4557c662c5b2a8622c6fcccbf31062b4ba589
igorliedlke <igorliedlke@gmail.com>
2025-09-28 00:15:27 -0300

    commit 1

---
+++ Spotify/script.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+
+
+
+const artistsData = [
+    { name: 'Henrique & Juliano', image: './img/artista-henrique-juliano.jpg' },
+    { name: 'Jorge & Mateus', image: './img/artista-jorge-mateus.jpg' },
+    { name: 'Zé Neto & Cristiano', image: './img/artista-ze-neto.jpg' },
+    { name: 'Gustavo Lima', image: './img/artista-gustavo-limma.jpg' },
+    { name: 'Luan Santana', image: './img/artista-luan-santana.jpg' },
+    { name: 'Mateus & Kauan', image: './img/artista-mateus-kauan.jpg' }
+]
+
+const albumsData = [
+    { name: 'White Noise (Sleep & Relaxation Sonds)', artist: 'Sleep John', image: './img/album-white-noise.jpg' },
+    { name: 'O céu explica tudo (Ao vivo)', artist: 'Henrique & Juliano', image: './img/album-ceu-explica.jpg' },
+    { name: 'Nada como um dia após o outro', artist: 'Racionais', image: './img/album-vida-loka.jpg' },
+    { name: 'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image: './img/album-hit-me.jpg' },
+    { name: 'CAJU', artist: 'Liniker', image: './img/album-caju.jpg' },
+    { name: 'Escandalo Íntimo', artist: 'Luísa Sonza', image: './img/album-escandalo.jpg' }
+]
+
+const artistGrid = document.querySelector('.artists-grid')
+const albumsGrid = document.querySelector('.albums-grid')
+
+artistsData.forEach( artist => {
+    const artistCard = document.createElement('div')
+    artistCard.classList.add('artist-card')
+
+    artistCard.innerHTML = `
+     <img src="${artist.image}" alt="imagem do ${artist.name}">
+     <button><i class="fa-solid fa-play"></i></button>
+     <h3>${artist.name}</h3>
+     <p>artista</p>
+    `
+    artistGrid.appendChild(artistCard)
+
+})
+
+    albumsData.forEach( album => {
+    const albumCard = document.createElement('div')
+    albumCard.classList.add('album-card')
+
+    albumCard.innerHTML = `
+     <img src="${album.image}" alt="imagem do ${album.name}">
+     <button><i class="fa-solid fa-play"></i></button>
+     <h3>${album.name}</h3>
+     <p>${album.artist}</p>
+    `
+    albumsGrid.appendChild(albumCard)
+    
+})
+})
